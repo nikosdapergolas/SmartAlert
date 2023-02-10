38d5dcec356919dab0c014934747e50d03f8abd1
Eva Mpouloudenou <mpouloudenou@gmail.com>
2023-02-11 00:35:02 +0200

    updated UI for danger submit from user

--- app/src/main/java/com/unipi/nickdap/p19039/smartalert/MainActivity5.java
+++ app/src/main/java/com/unipi/nickdap/p19039/smartalert/MainActivity5.java
@@ -3,12 +3,24 @@ package com.unipi.nickdap.p19039.smartalert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity5 extends AppCompatActivity {
+
+
+    Spinner spinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity5.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.dangers));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
     }
 }
